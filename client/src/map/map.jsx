8c59bd562981bd41fac7cd5f451163e5f38065d3
Davi <davipmello@hotmail.com>
2021-03-04 14:27:03 -0300

    Criação projeto base

---
+++ client/src/map/map.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {MapContainer, TileLayer, Marker, Popup, Circle} from 'react-leaflet';
+
+export default class Map extends React.Component {
+    render(){
+        return <MapContainer center={[64.128701,-21.942068]} zoom={15} scrollWheelZoom={true}>
+        <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+            <Marker position={[64.128701,-21.942068]}>
+                <Popup>
+                    Reykjavíkurflugvöllur
+                </Popup>
+            </Marker>
+            <Circle center={[64.128701,-21.942068]} radius={900}>
+                <Popup>
+                    Reykjavíkurflugvöllur&apos;s region
+                </Popup>
+            </Circle>
+        </MapContainer>
+    }
+}
---
+++ client/src/views/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Map from '../map/map'
+
+class Index extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Map/>
+      </div>
+    );
+  }
+}
+
+export default Index;
